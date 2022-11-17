a55d6d68b42a503a2764f69b7876d810b242fef4
MrPreet <harpreet9629@gmail.com>
2022-11-17 21:24:38 +0530

    Fixed Video Title

--- src/utils/functions/getVideoTitle.js
+++ src/utils/functions/getVideoTitle.js
@@ -1,6 +1,6 @@
 export const getVideoTitle = (video, profile = null) => {
     const videoProfile = video.ProfileEntryResponse;
     const payload = video.PostExtraData?.Videso ? JSON.parse(video.PostExtraData.Videso) : null;
-    const title = (payload !== null && payload.Title !== '') ? payload.Title : `Video by ${profile?.Username}`;
-    return title;
+    const title = (payload !== null && payload.Title !== '') ? payload.Title : profile !== null ? profile.Username : videoProfile.Username;
+    return `Video by ${title}`;
 }
