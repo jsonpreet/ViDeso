1160770b3d082cece9844194beabf8083022f6c4
MrPreet <harpreet9629@gmail.com>
2022-11-21 02:00:20 +0530

    Fixing Errors

--- src/utils/functions/getProfilePicture.js
+++ src/utils/functions/getProfilePicture.js
@@ -1,6 +1,7 @@
 export const getProfilePicture = (channel) => {
+  console.log('channel', channel);
   const channelExtra = channel.ExtraData || {};
   const largeImage = channelExtra.LargeProfilePicURL !== null ? channelExtra.LargeProfilePicURL  : null
-  const url = largeImage ? largeImage : `https://diamondapp.com/api/v0/get-single-profile-picture/${channel.PublicKeyBase58Check}`
+  const url = largeImage ? largeImage : `https://diamondapp.com/api/v0/get-single-profile-picture/${channel.PublicKeyBase58Check}?fallback=https://diamondapp.com/assets/img/default_profile_pic.png`
   return url;
 }
