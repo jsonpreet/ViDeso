b3d1de5ec561433870294fe251d043878dcfa344
MrPreet <harpreet9629@gmail.com>
2022-12-01 20:47:52 +0530

    Pagination added for Popular Feed

--- src/components/Explore/Category/Category.jsx
+++ src/components/Explore/Category/Category.jsx
@@ -1,5 +1,4 @@
 import VideoCard from '@app/components/Common/VideoCard/VideoCard'
-import { FetchHotFeed, FetchInfiniteHotFeed } from '@app/data/videos';
 import { useInView } from 'react-intersection-observer'
 import { useEffect } from 'react';
 import Custom500 from '@app/pages/404';
@@ -9,48 +8,65 @@ import MetaTags from '@app/components/Common/MetaTags';
 import TimelineShimmer from '@app/components/Shimmers/TimelineShimmer';
 import { APP } from '@app/utils/constants';
 import { getShuffleArray } from '@app/utils/functions/getShuffleArray';
-import { GetHotFeed } from '@app/data/hot';
+import { FetchInfiniteHotFeed, GetHotFeed } from '@app/data/hot';
+import { Loader2 } from '@app/components/UIElements/Loader';
 
 function Category() {
-    const { user, isLoggedIn } = usePersistStore();
     const { ref, inView } = useInView()
-    const reader = user.profile ? user.profile.PublicKeyBase58Check : APP.PublicKeyBase58Check;
-    const { isError, error, isLoading, isFetched, data:videos } = GetHotFeed( reader );
-    
-    if (videos?.length === 0) {
-        return (
-            <NoDataFound
-                isCenter
-                withImage
-                text="We are unable to fetch the videos. Please try again later."
-            />
-        )
-    }
+    const user = usePersistStore((state) => state.user)
+    const isLoggedIn = usePersistStore((state) => state.isLoggedIn)
+    const reader = isLoggedIn ? user.profile.PublicKeyBase58Check : APP.PublicKeyBase58Check;
+    const { isError, error, isSuccess, hasNextPage, isFetchingNextPage, fetchNextPage, data } = FetchInfiniteHotFeed(reader);  
+
+    useEffect(() => {
+        if (inView && hasNextPage) {
+            fetchNextPage()
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [inView, hasNextPage])
 
     if (isError) {
-        console.log(error)
-        return <Custom500 />
+        return <NoDataFound 
+        isCenter
+        withImage
+        title="Something went wrong"
+        description="We are unable to fetch the latest videos. Please try again later."
+        />
     }
-
-    if (isLoading) {
-        return <div><TimelineShimmer cols={28} /></div>
-    }
-
-    if (isFetched) {
-        return (
+    return (
+        <>
+        {
+            isSuccess ? (
             <>
-                <MetaTags title="Feed" />
                 <div className="grid gap-x-4 lg:grid-cols-4 md:gap-y-4 gap-y-2 2xl:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 xs:grid-col-1">
-                    {getShuffleArray(videos).map(video => {
-                            return (
-                                <VideoCard userProfile={video.ProfileEntryResponse} key={`${video.PostHashHex}`} video={video} />
-                            )
-                        })
-                    }
+                {data.pages.map(page =>
+                    getShuffleArray(page.posts).map(video => {
+                    return (
+                        <VideoCard userProfile={video.ProfileEntryResponse} key={`${video.PostHashHex}`} video={video} />
+                    )
+                    }))}
+                </div>
+                
+                <div className='loadMore flex items-center justify-center mt-10'>
+                <div className='loadMoreButton'>
+                    <div ref={ref} onClick={fetchNextPage} disabled={!hasNextPage || isFetchingNextPage} className='btn'>
+                    {isFetchingNextPage
+                        ? <Loader2 />
+                        : hasNextPage
+                        ? 'Load More'
+                        : 'Nothing more to load'}
+                    </div>
+                </div>
                 </div>
             </>
-        )
-    }
+            )
+            : (
+            <div><TimelineShimmer cols={28} /></div>
+            )
+        }
+        </>
+        
+    )
 }
 
 export default Category
