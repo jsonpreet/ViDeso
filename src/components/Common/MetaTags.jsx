4af1f09aeb8bf3530a593aa0c2bf5116c5d9fcc1
MrPreet <harpreet9629@gmail.com>
2022-12-01 20:00:02 +0530

    Meta Tags

--- src/components/Common/MetaTags.jsx
+++ src/components/Common/MetaTags.jsx
@@ -9,7 +9,7 @@ const MetaTags = (props) => {
   const meta = {
     title: title ? `${title} - ${APP.Name}` : APP.Name,
     description: description ?? APP.Description,
-    image: image ?? `/images/seo/og.png`,
+    image: image ?? `/meta.png`,
     type: 'website'
   }
 
@@ -28,8 +28,16 @@ const MetaTags = (props) => {
       <meta property="og:title" content={meta.title} />
       <meta property="og:image:width" content="1200"/>
       <meta property="og:image:height" content="630"/>
-      <meta property="og:image:type" content="image/jpeg"/>
-      <meta property="og:image:alt" content={meta.description}/>
+      <meta property="og:image:type" content="image/png"/>
+      <meta property="og:image:alt" content={meta.description} />
+      <meta name="twitter:card" content="summary" />
+      <meta property="twitter:image:width" content="400" />
+      <meta property="twitter:image:height" content="400" />
+      <meta name="twitter:site" content="VidesoApp" />
+      <meta name="twitter:title" content={meta.title} />
+      <meta name="twitter:description" content={meta.description} />
+      <meta property="twitter:image:src" content={meta.image} />
+      <meta property="twitter:creator" content='@VidesoApp' />
     </Head>
   )
 }
